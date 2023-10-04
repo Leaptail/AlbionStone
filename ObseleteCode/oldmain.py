0a088493f6707ce2494e4475194b8c950f9467bc
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-10-04 13:04:01 +0800

    Setting off old code to a side

---
+++ ObseleteCode/oldmain.py
@@ -0,0 +1,23 @@
+import time
+import pyautogui 
+import pydirectinput
+import cv2 as cv
+import numpy as np
+import win32gui, win32ui, win32con
+from windowcap import window_cap
+
+breakey = 'p'
+#haystack_img = cv.imread('farm.PNG', cv.IMREAD_UNCHANGED)
+#needle_img = cv.imread('memories/Tree/Tree1.PNG', cv.IMREAD_UNCHANGED)
+wincap = window_cap('New Tab - Google Chrome')
+
+while True:
+
+    screenshots = wincap.getssh()
+    cv.imshow('Vision', screenshots)
+
+    if cv.waitKey(1) == ord('p'):
+        cv.destroyAllWindows()
+        break
+
+cv.destroyAllWindows()
--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 import cv2 as cv
 import numpy as np
 from time import time
-from windowcap import window_cap
 from githubss import capture_win_alt
 
-# Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their own folder on GitHub
+# Terminates program when held
+terminalbutton = 'p'
 
-
-# initialize the WindowCapture class
+# Insert Name of window here
 wincap = capture_win_alt('New Tab - Google Chrome')
 
 loop_time = time()
+
 while(True):
 
     # get an updated image of the game
@@ -26,38 +25,8 @@ while(True):
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(1) == ord(terminalbutton):
         cv.destroyAllWindows()
         break
 
-print('Done.')
-
-"""
-
-import time
-import pyautogui 
-import pydirectinput
-import cv2 as cv
-import numpy as np
-import win32gui, win32ui, win32con
-from windowcap import window_cap
-
-breakey = 'p'
-#haystack_img = cv.imread('farm.PNG', cv.IMREAD_UNCHANGED)
-#needle_img = cv.imread('memories/Tree/Tree1.PNG', cv.IMREAD_UNCHANGED)
-wincap = window_cap('New Tab - Google Chrome')
-
-while True:
-
-    screenshots = wincap.getssh()
-    cv.imshow('Vision', screenshots)
-
-    if cv.waitKey(1) == ord('p'):
-        cv.destroyAllWindows()
-        break
-    
-cap.release()
-cv.destroyAllWindows()
-"""
-
-
+print('Finnish.')
