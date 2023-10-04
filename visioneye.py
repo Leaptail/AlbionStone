027db4648f05bc406f8929f65cc0af6d7eb17192
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-10-04 12:38:54 +0800

    AlbionStoner Repo

---
+++ visioneye.py
@@ -0,0 +1,79 @@
+import time
+import pyautogui 
+import pydirectinput
+import cv2 as cv
+import numpy as np
+
+"""
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+print(result)
+
+threshold = 0.3
+locations = np.where(result >= threshold)
+print(locations)
+
+locations = list(zip(*locations[::-1]))
+print(locations)
+
+if locations:
+    print('found')
+
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    line_color = (255,0,0)
+    line_type = cv.LINE_4
+
+    for loc in locations:
+        top_left = loc
+        bottom_right = (top_left[0]+needle_w,top_left[1]+needle_h)
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+
+    cv.imshow('Result', haystack_img)
+    cv.waitKey()
+else:
+    print('not found')
+
+
+
+
+
+
+#Debug code
+#cv.imshow('Result', result)
+#cv.waitKey()
+
+"""
+
+
+
+
+
+
+
+
+
+#min max of brightest and darkest pixel, get best match position
+"""min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+print('%s' %str(max_loc))
+print('%s' %str(max_val))
+
+threshold = 0.8
+if max_val >= threshold:
+    print('found')
+
+    #get dimension of needle image
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+
+    top_left = max_loc
+    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+
+    cv.rectangle(haystack_img, top_left, bottom_right, color=(255,0,0), thickness=2, lineType=cv.LINE_4)
+    print('%d , %d' % top_left, bottom_right)
+
+    cv.imshow('Result', haystack_img)
+    cv.waitKey()
+
+else:
+    print('none')"""
---
+++ windowcap.py
@@ -0,0 +1,55 @@
+import numpy as np
+import win32gui, win32ui, win32con
+
+class window_cap():
+     #window width height
+    w = 1920
+    h = 1080
+    hwnd = None
+    #constructor
+    def __init__(self, window_name):
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception('Window not found: {}'.format(window_name))
+        self.w = 1920
+        self.h = 1080
+
+    def getssh(self):
+       
+        #getting winapi to ss/speific window
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj=win32ui.CreateDCFromHandle(wDC)
+        cDC=dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0,0),(self.w, self.h) , dcObj, (0,0), win32con.SRCCOPY) #problem is somewhere here
+        #change it to be readable by opencv
+        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h,self.w,4)
+        #free space and resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        #drop alphachannel/transparency
+        img = img[...,:3]
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    def list_window_names(self):
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
+
+    # translate a pixel position on a screenshot image to a pixel position on the screen.
+    # pos = (x, y)
+    # WARNING: if you move the window being captured after execution is started, this will
+    # return incorrect coordinates, because the window position is only calculated in
+    # the __init__ constructor.
+    def get_screen_position(self, pos):
+        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
